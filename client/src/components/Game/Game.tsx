6e66f45bbd2ba8ea959c03fc2b816c7b56aafa6a
Gregory Roman <gregorymichaelroman@gmail.com>
2021-10-24 17:39:40 -0400

    Working on game state.

--- client/src/components/Game/Game.tsx
+++ client/src/components/Game/Game.tsx
@@ -1,9 +1,9 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState, useMemo } from 'react';
 import { AppContext } from '../../contexts';
-import { useCreateGameMutation } from '../../graphql/generated/types';
+import { Answer, Survey, useCreateGameMutation, useSurveysQuery } from '../../graphql/generated/types';
 import { ActionTypes } from '../../store';
 
-import './GameBoard.css';
+import './Game.css';
 
 export const Game: React.FC = () => {
   const { dispatch, state: { currentGame } } = useContext(AppContext);
@@ -49,8 +49,6 @@ export const Game: React.FC = () => {
 }
 
 
-const Host: React.FC = () => {
-  const [strikes, setStrikes] = useState(0);
 /* 
 Survey
 Host
@@ -67,28 +65,80 @@ Player {
 }
 */
 
+enum GameStatus {
+  InProgress = 'inProgress',
+  Lose = 'lose',
+  NotStarted = 'notStarted',
+  Steal = 'steal',
+  Win = 'win',
+}
 
+const Host: React.FC = () => {
+  const [strikes, setStrikes] = useState(0);
+  const [correctAnswers, setCorrectAnswers] = useState<Answer['id'][]>([]);
+  const { data } = useSurveysQuery();
 
+  const survey = data?.surveys[0];
 
+  const gameStatus: GameStatus = useMemo(() => {
+    if (!survey) {
+      return GameStatus.NotStarted;
+    }
+    if (strikes > 2) {
+      return GameStatus.Lose;
+    }
+    if (correctAnswers.length === survey.answers.length) {
+      return GameStatus.Win;
+    }
+    return GameStatus.InProgress;
+  }, [
+    correctAnswers, 
+    survey,
+    strikes
+  ]);
+
+  const isGameOver: boolean = useMemo(() => [
+    GameStatus.Win, 
+    GameStatus.Lose
+  ].includes(gameStatus), [gameStatus]);
 
   return (
-    <div>
+    <div className="game">
+      <div>
+        <h2>Survey {gameStatus === GameStatus.Win ? 'winner' : 'in progress'}</h2>
+        <div className="answers">
+          {
+            survey && survey.answers.map(answer => (
+              <label className="answer" key={answer.id}>
+                <span>{answer.text}</span>
+                <input 
+                  type="checkbox" 
+                  disabled={isGameOver ?? correctAnswers.includes(answer.id)}
+                  onChange={(e) => setCorrectAnswers(answers => [...answers, answer.id])}
+                />
+              </label>
+            ))
+          }
+        </div>
+      </div>
       <div>
         Strikes: {strikes}
       </div>
       {
-        strikes < 3 ? (
-          <div>
-            <button>Correct</button>
-            <button onClick={() => setStrikes(strikes => strikes + 1)}>
-              Wrong
-            </button>
-          </div>
-        ): (
+        isGameOver ? (
           <div>
             <h2>Round Over</h2>
-            <button onClick={() => setStrikes(0)}>
+            <button onClick={() => {
+              setStrikes(0);
+              setCorrectAnswers([]);
+            }}>
               Start Next Round
+            </button>
+          </div>          
+        ): (
+          <div>
+            <button onClick={() => setStrikes(strikes => strikes + 1)}>
+              STRIKE
             </button>
           </div>
         )
