d43cba567749ac4b62b6172c01b79800804813e8
Gregory Roman <gregorymichaelroman@gmail.com>
2021-11-14 20:09:37 -0500

    db config path issue.

--- server/database/config/config.js
+++ server/database/config/config.js
@@ -1,10 +1,9 @@
-{
+const path = require('path');
+
+module.exports = () => ({
   "development": {
-    "username": "root",
-    "password": null,
-    "database": "database_development",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    "dialect": "sqlite",
+    "storage": path.join(__dirname, '../store.sqlite')
   },
   "test": {
     "username": "root",
@@ -20,4 +19,4 @@
     "host": "127.0.0.1",
     "dialect": "mysql"
   }
-}
+});
